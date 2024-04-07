951be9f50ca107009e4c442cf144d7597822f5be
Safari Developer <142569922+SafariDeveloper@users.noreply.github.com>
2024-04-07 22:34:06 +0530

    Delete SAFARI directory

--- SAFARI/__init__.py
+++
@@ -1,8 +0,0 @@
-from aiohttp import web
-from SAFARI.stream_routes import routes
-
-
-async def web_server():
-    web_app = web.Application(client_max_size=30000000)
-    web_app.add_routes(routes)
-    return web_app
