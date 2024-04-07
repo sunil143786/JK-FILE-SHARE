5ee4249536e088ef095d290a409d871118f2b783
Safari Developer <142569922+SafariDeveloper@users.noreply.github.com>
2024-04-07 23:20:27 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -67,7 +67,6 @@ async def start():
     b_users, b_chats = await db.get_banned()
     temp.BANNED_USERS = b_users
     temp.BANNED_CHATS = b_chats
-    await super().start()
     await Media.ensure_indexes()
     me = await SafariBot.get_me()
     temp.ME = me.id
