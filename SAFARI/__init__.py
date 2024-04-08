ef71bb1f07dae3dc5c62601ca5e91ee40f5fba74
Safari Developer <142569922+SafariDeveloper@users.noreply.github.com>
2024-04-08 12:24:44 +0530

    Update __init__.py

--- SAFARI/__init__.py
+++ SAFARI/__init__.py
@@ -1,13 +1,5 @@
 import time
-from aiohttp import web
-from SAFARI.route import routes
-
 
 StartTime = time.time()
 __version__ = 1.1
 
-
-async def web_server():
-    web_app = web.Application(client_max_size=30000000)
-    web_app.add_routes(routes)
-    return web_app
